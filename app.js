36724b60b5bfd0965690b66d0c7b43de4da3ff11
hexyminx <hexyminx@gmail.com>
2022-12-01 12:27:35 -0500

    added editing function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 //Load config
@@ -17,13 +18,27 @@ connectDB()
 
 const app = express()
 
+//Body parser
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
 //Logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
+//Handlebars helpers
+const {formatDate, stripTags, truncate, editIcon, select} = require('./routes/helpers/hbs')
+
 //Handlebars
 app.engine('.hbs', exphbs.engine({
+    helpers:{
+        formatDate,
+        stripTags,
+        truncate,
+        editIcon,
+        select,
+    },
     defaultLayout: 'main',
     extname: '.hbs'
     })
@@ -31,10 +46,14 @@ app.engine('.hbs', exphbs.engine({
 app.set('view engine', '.hbs')
 
 //Session middleware
-app.use(session({
-    secret: 'keyboard-cat',
-    resave: false,
-    saveUninitialized: false,
+app.use(
+    session({
+        secret: 'keyboard-cat',
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGO_URI
+        })
     })
 )
 
@@ -42,13 +61,19 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+//Set global variable
+app.use(function (req, res, next){
+    res.locals.user = req.user || null
+    next()
+})
+
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
-
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 5000
 
